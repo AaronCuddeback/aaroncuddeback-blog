3d2f626af79c77ba49c7392f0bf1e8822ef42187
Aaron Cuddeback <aaron.cuddeback@outlook.com>
2019-12-09 13:09:45 -0800

    add new level

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import _ from 'lodash';
 import {safePrefix} from '../utils';
 import Header from './Header';
 import Footer from './Footer';
-import favicon from "../static/images/favicon.png";
+import favicon from "../../static/images/favicon.png";
 
 export default class Body extends React.Component {
     render() {
